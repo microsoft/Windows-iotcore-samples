9312377fa4e97380ddcebfc12ab0d53a3c6b1c3f
Sara <hi@saraclayton.co>
2017-12-14 16:56:48 -0800

    Adding Building Navigation Robot

---
+++ Demos/BuildingNavigationRobot/CS/Helpers/Telemetry.cs
@@ -0,0 +1,64 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+
+using Microsoft.Azure.Devices.Client;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureIoTHub
+{
+    class Telemetry
+    {
+        private static string DeviceId = "YOUR_DEVICE_ID";
+        private static string HubUri = "YOUR_AZURE_IOT_HUB_URI";
+        private static string ConnectionKey = "YOUR_AZURE_IOT_HUB_CONNECTION_KEY";
+
+        private static DeviceClient deviceClient;
+
+        public static async void SendReport(MessageType type, string message)
+        {
+            var guid = Guid.NewGuid();
+
+            if (deviceClient == null)
+            {
+                Debug.WriteLine("Created new instance of DeviceClient!");
+                deviceClient = DeviceClient.Create(HubUri,
+                    AuthenticationMethodFactory.CreateAuthenticationWithRegistrySymmetricKey(DeviceId, ConnectionKey),
+                    TransportType.Http1);
+            }
+
+            var str = $"{{\"deviceId\":\"{DeviceId}\",\"guid\":\"{guid.ToString()}\",\"type\":\"{TypeString(type)}\",\"message\":\"{message}\"}}";
+
+            var messageJson = new Message(Encoding.ASCII.GetBytes(str));
+
+            await deviceClient.SendEventAsync(messageJson);
+            Debug.WriteLine("Telemetry data sent: {0} -> {1}", TypeString(type), message);
+        }
+
+        private static string TypeString(MessageType type)
+        {
+            switch (type)
+            {
+                case MessageType.VoiceCommand:
+                    return "VoiceCommand";
+                case MessageType.Navigation:
+                    return "Navigation";
+                case MessageType.Positioning:
+                    return "Positioning";
+                case MessageType.Collision:
+                    return "Collision";
+                default:
+                    return "Error error";
+            }
+        }
+
+        public enum MessageType
+        {
+            VoiceCommand, Navigation, Positioning, Collision
+        }
+
+    }
+}
---
+++ Demos/BuildingNavigationRobot/README.md
@@ -0,0 +1,16 @@
+# Windows IoT Building Navigation Robot
+The Windows IoT Building Navigation Robot is a robot that will navigate a person to a conference room or office in a building.  The robot consists of an iRobot Create 2 with a LIDAR, screen, battery, and a device running Windows 10 IoT Core mounted on top.
+
+## Cloning the project
+
+- Use Command Prompt to navigate to the folder where you want the project:  
+  ```cd <your folder path>```  
+- Run the git clone command:  
+  ```git clone https://github.com/ms-iot/building-navigation-robot.git```  
+
+The step-by-step instruction for building this project can be found on https://www.hackster.io/54243/iot-core-building-navigation-robot-125856
+
+
+
+
+This project has adopted the [Microsoft Open Source Code of Conduct](https://opensource.microsoft.com/codeofconduct/). For more information see the [Code of Conduct FAQ](https://opensource.microsoft.com/codeofconduct/faq/) or contact [opencode@microsoft.com](mailto:opencode@microsoft.com) with any additional questions or comments. 
