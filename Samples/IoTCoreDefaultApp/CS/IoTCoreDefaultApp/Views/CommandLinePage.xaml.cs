00d576eea18d155d739dcc09cebb46c9c3b3be93
Mahmoud Saleh <msaleh@microsoft.com>
2018-02-12 09:52:55 -0800

    Display usage for runasadmin in more cases
    Commands supplied to runasadmin surrounded with doubcle quotes, such as "dir" will not parse correctly. So, prevent those. Additionally, disaplay usage in cass such as -? and /?

--- Samples/IoTCoreDefaultApp/CS/IoTCoreDefaultApp/Views/CommandLinePage.xaml.cs
+++ Samples/IoTCoreDefaultApp/CS/IoTCoreDefaultApp/Views/CommandLinePage.xaml.cs
@@ -168,7 +168,7 @@ namespace IoTCoreDefaultApp
         private void RunAdminCommand(string commandLineText)
         {
             adminCommandLine = commandLineText.Remove(0, "RunAsAdmin".Length).Trim();
-            if (adminCommandLine.Length == 0)
+            if (adminCommandLine.Length == 0 || adminCommandLine.StartsWith("-") || adminCommandLine.StartsWith("/") || adminCommandLine.StartsWith("\""))
             {
                 ShowError(resourceLoader.GetString("RunAsAdminUsage"));
                 EnableCommandLineTextBox(true, CommandLine);
