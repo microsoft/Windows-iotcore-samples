f16f2fcfe79d2c8a0a45d5eadf431045386f54d1
Jiong Shi <shijiong@outlook.com>
2018-03-09 16:56:57 +0800

    Add RPiCognitiveService Demo
    Add RPiCognitiveService Demo

---
+++ Samples/RPiCognitiveService/CS/RPiCognitiveServie/EmotionDataControl.xaml.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace RPiCognitiveServie
+{
+    public sealed partial class EmotionDataControl : UserControl
+    {
+        public EmotionDataControl()
+        {
+            this.InitializeComponent();
+            Loaded += EmotionDataControl_Loaded;
+        }
+
+        /// <summary>
+        /// emotion数据 id-data
+        /// </summary>
+        public Dictionary<string, double> Data
+        {
+            get; set;
+        }
+
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void EmotionDataControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (Data != null)
+            {
+                var w = this.ActualWidth;
+                var h = this.ActualHeight;
+
+                if (w <= 20)
+                    return;
+                Line x_axis = new Line();
+                x_axis.StrokeThickness = 1;
+                x_axis.Stroke = new SolidColorBrush(Colors.Orange);
+                Canvas.SetLeft(x_axis, 20);
+                Canvas.SetTop(x_axis, h - 20);
+                x_axis.Width = w - 40;
+
+                mainCanvas.Children.Add(x_axis);
+
+                var gap_w = (w - 40) / (Data.Count - 1);
+                var height = h - 40;
+
+                int index = 0; PointCollection list = new PointCollection();
+                list.Add(new Point(20, h - 20));
+                foreach (var p in Data)
+                {
+                    Point point = new Point(20 + index * gap_w, 20 + (height - height * p.Value));
+
+                    TextBlock txt = new TextBlock();
+                    txt.FontSize = 12;
+                    txt.Text = p.Key;
+
+                    Canvas.SetLeft(txt, point.X - 10);
+                    Canvas.SetTop(txt, point.Y - 20);
+                    mainCanvas.Children.Add(txt);
+
+                    list.Add(point);
+
+                    index++;
+                }
+                list.Add(new Point(w - 20, h - 20));
+
+                Polygon po = new Polygon();
+                po.Stroke = new SolidColorBrush(Colors.Orange);
+                po.StrokeThickness = 1;
+                po.Points = list;
+                po.Fill = new SolidColorBrush(Colors.Orange);
+                Canvas.SetZIndex(po, -1);
+                mainCanvas.Children.Add(po);
+            }
+        }
+    }
+}
---
+++ Samples/RPiCognitiveService/README.md
@@ -0,0 +1,20 @@
+
+# RPiCognitiveService
+Microsoft Cognitive Service (Emotion, Face and Computer Vision) Demo UWP App for RPi with Windows 10 IoT Core.
+# Version 2
+The camera captured pictures are now supported in this version.
+![image](https://github.com/shijiong/RPiCognitiveService/blob/master/Screenshot_2.jpg)
+# Version 1
+![image](https://github.com/shijiong/RPiCognitiveService/blob/master/Screenshot.jpg)
+# Hardware Requirement
+Network Access for RPi.
+LifeCam HD-3000 (Version 2)
+# Build and Run
+Open project with Visual Studio 2017 with Universal Windows App Development Tools installed;
+Choose ARM and Remote Machine, input the IP address of RPi;
+Copy and Paste your Emotion, Face and Computer Vision API Key from Azure Portal to PacePage.xaml.cs and PhotoPage.xaml.cs;
+Press Debug and run.
+# Work to do
+Add Camera based Cognitive Service in the near future.
+# Thanks
+This work is achieved based on the ComputerVision.UWP by sherlockchou86 (https://github.com/sherlockchou86/ComputerVision.UWP).
