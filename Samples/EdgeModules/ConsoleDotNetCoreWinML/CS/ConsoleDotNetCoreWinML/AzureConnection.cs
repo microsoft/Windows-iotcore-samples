e024a63803f49e4be7931e92c9a2cc25bc5ecea2
Sean McDowell <seanmcd@microsoft.com>
2019-01-16 17:49:12 -0800

    additional azure connection refactoring for winml sample. everything builds. nothing tested.

---
+++ Samples/EdgeModules/ConsoleDotNetCoreWinML/CS/ConsoleDotNetCoreWinML/AzureConnection.cs
@@ -0,0 +1,45 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+//
+
+using EdgeModuleSamples.Common.Azure;
+using EdgeModuleSamples.Common.Logging;
+using EdgeModuleSamples.Common.Messages;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDotNetCoreWinML
+{
+    public class AzureModule : AzureModuleBase {
+        public AzureModule() { }
+    };
+    public class AzureDevice : AzureDeviceBase
+    {
+        public AzureDevice() { }
+    };
+    public class AzureConnection : AzureConnectionBase
+    {
+        public AzureConnection() { }
+        public static async Task<AzureConnection> CreateAzureConnectionAsync()
+        {
+            return await CreateAzureConnectionAsync<AzureConnection, AzureDevice, AzureModule>();
+        }
+
+        private async Task UpdateObjectAsync(string fruit)
+        {
+            // output the event stream
+            var msgvalue = new FruitMessage();
+            msgvalue.FruitSeen = fruit;
+            byte[] msgbody = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msgvalue));
+            var m = new Message(msgbody);
+            await Module.SendMessageAsync("OutputFruit", m);
+            // Update the module twin
+        }
+    }
+}
