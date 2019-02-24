cbc7827e0742a4b6c7b419109b8a2a20924cedf8
James Coliz <jcoliz@mail.com>
2019-02-23 20:12:59 -0800

    Initially open I2C device

--- Samples/EdgeModules/I2CTempHumidity/CS/AppOptions.cs
+++ Samples/EdgeModules/I2CTempHumidity/CS/AppOptions.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 
+using EdgeModuleSamples.Common;
+
 namespace SampleModule
 {
     public class AppOptions: OptionSet
@@ -10,6 +12,7 @@ namespace SampleModule
         public bool ShowList { get; private set; }
         public bool ShowConfig { get; private set; }
         public string DeviceId { get; private set; }
+        public string DeviceAddress { get; private set; }
         public bool UseEdge { get; private set; }
         public bool Exit { get; private set; } = false;
 
@@ -17,9 +20,11 @@ namespace SampleModule
         {
             Add( "h|help", "show this message and exit", v => Help = v != null );
             Add( "l|list", "list available devices and exit", v => ShowList = v != null);
-            Add( "d|device=", "the {ID} of device to connect", v => DeviceId = v);
+            Add( "d|device=", "the {ID} of i2c controller to use", v => DeviceId = v.ToLowerInvariant());
+            Add( "a|address=", "the {ID} of the device address", v => DeviceAddress = v);
             Add( "c|config", "display device configuration", v => ShowConfig = v != null);
             Add( "e|edge", "transmit through azure edge", v => UseEdge = v != null);
+            Add( "v|verbose", "print verbose logging information", v => Log.Verbose = v != null);
         }
 
         public new List<string> Parse(IEnumerable<string> args)
--- Samples/EdgeModules/I2CTempHumidity/CS/Program.cs
+++ Samples/EdgeModules/I2CTempHumidity/CS/Program.cs
@@ -11,8 +11,12 @@ namespace SampleModule
     using System.Linq;
     using Microsoft.Azure.Devices.Client;
     using Newtonsoft.Json;
+    using Windows.Devices.Enumeration;
+    using Windows.Devices.Gpio;
+    using Windows.Devices.I2c;
 
     using EdgeModuleSamples.Common;
+    using static EdgeModuleSamples.Common.AsyncHelper;
 
     class Program
     {
@@ -20,7 +24,7 @@ namespace SampleModule
         static ModuleClient ioTHubModuleClient;
         static readonly Random Rnd = new Random();
 
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             try
             {
@@ -36,17 +40,62 @@ namespace SampleModule
                 // Enumerate devices
                 //
 
-                // ...
+                var devicepaths = await AsAsync(DeviceInformation.FindAllAsync(I2cDevice.GetDeviceSelector(), new[] { "System.DeviceInterface.Spb.ControllerFriendlyName" }));
+                if (Options.ShowList || string.IsNullOrEmpty(Options.DeviceId))
+                {
+                    if (devicepaths?.FirstOrDefault() != null)
+                    {
+                        Log.WriteLine("Available devices:");
+
+                        foreach (var device in devicepaths)
+                        {
+                            Console.WriteLine(device.Id);
+                        }
+                    }
+                    else
+                    {
+                        Log.WriteLine("There are no I2C controllers on this system");
+                    }
+                    return;
+                }
 
                 //
                 // Open Device
                 //
 
-                // ...
+                string aqs = string.Empty;
+                aqs = I2cDevice.GetDeviceSelector();
 
-
-                //using (...)
+#if nope
+                string aqs;
+                if (string.IsNullOrEmpty(Options.DeviceId))
+                    aqs = I2cDevice.GetDeviceSelector();
+                else
                 {
+                    var devicepath = devicepaths.Where(x=>x.Id.ToLowerInvariant().Contains(Options.DeviceId)).FirstOrDefault();
+
+                    if (null == devicepath)
+                        throw new ApplicationException($"Unable to find I2C Controller containing {Options.DeviceId}");
+
+                    Log.WriteLineVerbose($"Opening {devicepath.Id}...");
+
+                    aqs = I2cDevice.GetDeviceSelector(devicepath.Id);
+                }
+#endif
+                Log.WriteLineVerbose("Opening {0}...",aqs);
+                var deviceInfos = await AsAsync(DeviceInformation.FindAllAsync(aqs));
+
+                if (deviceInfos?.FirstOrDefault() == null)
+                    throw new ApplicationException("I2C controller not found");
+
+                var id = deviceInfos.First().Id;
+                Int32 slaveAddress = Convert.ToInt32(Options.DeviceAddress, 16);
+
+                using (var device = await AsAsync(I2cDevice.FromIdAsync(id, new I2cConnectionSettings(slaveAddress))) )
+                {
+                    if (null == device)
+                        throw new ApplicationException($"Slave address 0x{slaveAddress:X} on bus {id} is in use. Please ensure that no other applications are using this device.");
+
                     //
                     // Configure Device
                     //
@@ -57,11 +106,25 @@ namespace SampleModule
                     // Dump device info
                     //
 
-
                     if (Options.ShowConfig)
                     {
-                        // ...
+                        // Bus parameters
+
+                        Log.WriteLineRaw($"    Device Id: {device.DeviceId}");
+                        Log.WriteLineRaw($"Slave Address: {device.ConnectionSettings.SlaveAddress:X}");
+                        Log.WriteLineRaw($"    Bus Speed: {device.ConnectionSettings.BusSpeed}");
+                        Log.WriteLineRaw($" Sharing Mode: {device.ConnectionSettings.SharingMode}");
+
+                        // Device serial number
+
+                        var serial1 = I2CReadWrite(device, new byte[] { 0xfa, 0x0f }, 8 );
+                        var serial2 = I2CReadWrite(device, new byte[] { 0xfc, 0xc9 }, 6 );
+                        var firmwarerev = I2CReadWrite(device, new byte[] { 0x84, 0xb8 }, 1 );
                     }
+
+                    //
+                    // Get some readings
+                    //
 
                     //
                     // Init module client
@@ -72,7 +135,7 @@ namespace SampleModule
                         Init().Wait();
                     }
 
-
+#if nope
                     // Wait until the app unloads or is cancelled
                     //if (Options.Receive || Options.Transmit)
                     {
@@ -81,6 +144,7 @@ namespace SampleModule
                         Console.CancelKeyPress += (sender, cpe) => cts.Cancel();
                         WhenCancelled(cts.Token).Wait();
                     }
+#endif
                 }
             }
             catch (Exception ex)
@@ -89,7 +153,17 @@ namespace SampleModule
             }        
         }
 
+        private static byte[] I2CReadWrite(I2cDevice device, byte[] writeBuffer, int readsize)
+        {
+            byte[] readBuffer = new byte[readsize];
+            var result = device.WriteReadPartial(writeBuffer,readBuffer);
+            Log.WriteLineVerbose(result.ToString());
 
+            var display = readBuffer.Aggregate(new StringBuilder(),(sb,b)=>sb.Append(string.Format("{0:X} ",b)));
+            Log.WriteLineVerbose(display.ToString());
+
+            return readBuffer;
+        }
 
         /// <summary>
         /// Handles cleanup operations when app is cancelled or unloads
