5f5cb327661066a1cb637ec868dde5c245c8f317
chandde <chandde@microsoft.com>
2019-02-26 22:40:13 -0800

    Add files via upload

---
+++ Samples/ShellLauncherV2/ShellLauncherV2/SampleConfigXmls/ShellLauncherBridgeWmi.ps1
@@ -0,0 +1,22 @@
+# Save this ps1 file to your PC
+# Download ps internal tools, run "psexec.exe -i -s powershell.exe" from elevated command prompt
+
+# In the powershell launched by psexec.exe, first load the ps1 file, be aware the . command when importing the ps1 file
+# PS C:\Users\dengy> . .\ShellLauncherBridgeWmi.ps1
+
+# After importing, run the command SetShellLauncherBridgeWMI with FilePath parameter to a shell launcher config xml
+# PS C:\Users\dengy> SetShellLauncherBridgeWMI -FilePath .\ShellLauncher.xml
+
+function SetShellLauncherBridgeWMI
+{
+    param([Parameter(Mandatory=$True)][String] $FilePath)
+
+    $Xml = Get-Content -Path $FilePath
+    $EscapedXml = [System.Security.SecurityElement]::Escape($Xml)
+    $AssignedAccessCsp = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    $AssignedAccessCsp.ShellLauncher = $EscapedXml
+    Set-CimInstance -CimInstance $AssignedAccessCsp
+    
+    $AssignedAccessCsp1 = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    $AssignedAccessCsp1.ShellLauncher
+} 
