5235fe51f1e3962fb4a5814b9cdef6fd56e09853
chandde <chandde@microsoft.com>
2019-02-28 09:49:47 -0800

    Update ShellLauncherBridgeWmi.ps1

--- Samples/ShellLauncherV2/ShellLauncherV2/SampleConfigXmls/ShellLauncherBridgeWmi.ps1
+++ Samples/ShellLauncherV2/ShellLauncherV2/SampleConfigXmls/ShellLauncherBridgeWmi.ps1
@@ -1,11 +1,14 @@
 # Save this ps1 file to your PC
-# Download ps internal tools, run "psexec.exe -i -s powershell.exe" from elevated command prompt
+# Download SysInternals tools, run "psexec.exe -i -s powershell.exe" from elevated command prompt
 
 # In the powershell launched by psexec.exe, first load the ps1 file, be aware the . command when importing the ps1 file
 # PS C:\Users\test> . .\ShellLauncherBridgeWmi.ps1
 
-# After importing, run the command SetShellLauncherBridgeWMI with FilePath parameter to a shell launcher config xml
+# After importing, run the command SetShellLauncherBridgeWMI with FilePath pointing to a shell launcher config xml
+# this config xml is raw xml that's NOT escaped
 # PS C:\Users\test> SetShellLauncherBridgeWMI -FilePath .\ShellLauncher.xml
+
+# To clean up ShellLauncher using bridge WMI, run the other command ClearShellLauncherBridgeWMI
 
 function SetShellLauncherBridgeWMI
 {
@@ -19,4 +22,11 @@ function SetShellLauncherBridgeWMI
     
     $AssignedAccessCsp1 = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
     $AssignedAccessCsp1.ShellLauncher
-} 
+}
+
+function ClearShellLauncherBridgeWMI
+{
+    $AssignedAccessCsp = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    $AssignedAccessCsp.ShellLauncher = $NULL
+    Set-CimInstance -CimInstance $AssignedAccessCsp
+}
