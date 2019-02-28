8ea858c3cef400fc3726f4970b518e4b156c05a3
chandde <chandde@microsoft.com>
2019-02-28 10:16:44 -0800

    Update ShellLauncherBridgeWmiHelpers.ps1

--- Samples/ShellLauncherV2/SampleBridgeWmiScripts/ShellLauncherBridgeWmiHelpers.ps1
+++ Samples/ShellLauncherV2/SampleBridgeWmiScripts/ShellLauncherBridgeWmiHelpers.ps1
@@ -1,20 +1,33 @@
-function SetShellLauncherBridgeWMI
+$NameSpace = "root\cimv2\mdm\dmmap"
+$Class = "MDM_AssignedAccess"
+
+function Get-AssignedAccessCspBridgeWmi
+{
+    return Get-CimInstance -Namespace $NameSpace -ClassName $Class
+}
+
+function Set-ShellLauncherBridgeWMI
 {
     param([Parameter(Mandatory=$True)][String] $FilePath)
 
     $Xml = Get-Content -Path $FilePath
     $EscapedXml = [System.Security.SecurityElement]::Escape($Xml)
-    $AssignedAccessCsp = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    $AssignedAccessCsp = Get-AssignedAccessCspBridgeWmi
     $AssignedAccessCsp.ShellLauncher = $EscapedXml
     Set-CimInstance -CimInstance $AssignedAccessCsp
     
-    $AssignedAccessCsp1 = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
-    $AssignedAccessCsp1.ShellLauncher
+    # get a new instance and print the value
+    (Get-AssignedAccessCspBridgeWmi).ShellLauncher
 }
 
-function ClearShellLauncherBridgeWMI
+function Clear-ShellLauncherBridgeWMI
 {
-    $AssignedAccessCsp = Get-CimInstance -Namespace "root\cimv2\mdm\dmmap" -ClassName "MDM_AssignedAccess"
+    $AssignedAccessCsp = Get-AssignedAccessCspBridgeWmi
     $AssignedAccessCsp.ShellLauncher = $NULL
     Set-CimInstance -CimInstance $AssignedAccessCsp
+}
+
+function Get-ShellLauncherBridgeWMI
+{
+    (Get-AssignedAccessCspBridgeWmi).ShellLauncher
 }
