0ddf90b680b21e4f86e3755c40fdaadab0e814f8
Sean McDowell <seanmcd@microsoft.com>
2019-03-07 08:33:31 -0800

    checkpint initial PWM

---
+++ Samples/EdgeModules/pwm/CS/AzureConnection.cs
@@ -0,0 +1,251 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+//
+
+using EdgeModuleSamples.Common.Azure;
+using EdgeModuleSamples.Common.Logging;
+using EdgeModuleSamples.Common.Messages;
+using Microsoft.Azure.Devices.Client;
+using Microsoft.Azure.Devices.Client.Exceptions;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GPIOFruit
+{
+    public class AzureDevice : AzureDeviceBase
+    {
+        public AzureDevice() { }
+    }
+    [JsonObject(MemberSerialization.Fields)]
+    class ConfigurationType : BaseConfigurationType
+    {
+        public GpioPinIndexesType GpioPins;
+        public override bool Update(BaseConfigurationType newValue)
+        {
+            var v = (ConfigurationType)newValue;
+            Log.WriteLine("updating from {0} to {1}", this.ToString(), v.ToString());
+            bool rc = GpioPins.Update(v.GpioPins);
+            Log.WriteLine("{0} update to {1}", rc ? "did" : "did not", this.ToString());
+            return rc;
+        }
+        public override string ToString()
+        {
+            return String.Format("{0} {1}", GetType().Name, GpioPins.ToString());
+        }
+
+    }
+
+    class AzureModule : AzureModuleBase
+    {
+        private DateTime _lastFruitUTC;
+        private DateTime _lastOrientation0UTC;
+        private DateTime _lastOrientation1UTC;
+        private DesiredPropertiesType<ConfigurationType> _desiredProperties;
+        public ConfigurationType Configuration { get { return _desiredProperties.Configuration; } }
+        public event EventHandler<ConfigurationType> ConfigurationChanged;
+        public event EventHandler<string> FruitChanged;
+        public event EventHandler<EdgeModuleSamples.Common.Orientation> Orientation0Changed;
+        public event EventHandler<EdgeModuleSamples.Common.Orientation> Orientation1Changed;
+
+        // TODO: refactor setfruit and onfruitmessage to share common code
+        private Task<MethodResponse> SetFruit(MethodRequest req, Object context)
+        {
+            string data = Encoding.UTF8.GetString(req.Data);
+            Log.WriteLine("Direct Method SetFruit {0}", data);
+            var fruitMsg = JsonConvert.DeserializeObject<FruitMessage>(data);
+            DateTime originalEventUTC = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+            if (fruitMsg.OriginalEventUTCTime != null)
+            {
+                originalEventUTC = DateTime.Parse(fruitMsg.OriginalEventUTCTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+                Log.WriteLine("SetFruit invoking event. parsed msg time {0} from {1}", originalEventUTC.ToString("o"), fruitMsg.OriginalEventUTCTime);
+            }
+            else
+            {
+                Log.WriteLine("msg has no time.  using current {0}", originalEventUTC.ToString("o"));
+            }
+            if (originalEventUTC >= _lastFruitUTC)
+            {
+                Log.WriteLine("SetFruit invoking event. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), _lastFruitUTC.ToString("o"));
+                AzureModule module = (AzureModule)context;
+                module.FruitChanged?.Invoke(module, fruitMsg.FruitSeen);
+                _lastFruitUTC = originalEventUTC;
+            }
+            else
+            {
+                Log.WriteLine("SetFruit ignoring stale message. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), _lastFruitUTC.ToString("o"));
+            }
+            // Acknowlege the direct method call with a 200 success message
+            string result = "{\"result\":\"Executed direct method: " + req.Name + "\"}";
+            return Task.FromResult(new MethodResponse(Encoding.UTF8.GetBytes(result), 200));
+        }
+
+        private static async Task<MessageResponse> OnFruitMessageReceived(Message msg, object ctx)
+        {
+            AzureModule module = (AzureModule)ctx;
+            var msgBytes = msg.GetBytes();
+            var msgString = Encoding.UTF8.GetString(msgBytes);
+            Log.WriteLine("fruit msg received: '{0}'", msgString);
+            var fruitMsg = JsonConvert.DeserializeObject<FruitMessage>(msgString);
+            DateTime originalEventUTC = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+            if (fruitMsg.OriginalEventUTCTime != null)
+            {
+                originalEventUTC = DateTime.Parse(fruitMsg.OriginalEventUTCTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+            }
+            else
+            {
+                Log.WriteLine("msg has no time.  using current {0}", originalEventUTC.ToString("o"));
+            }
+
+            if (originalEventUTC >= module._lastFruitUTC)
+            {
+                Log.WriteLine("FruitMsgHandler invoking event. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastFruitUTC.ToString("o"));
+                await Task.Run(() => module.FruitChanged?.Invoke(module, fruitMsg.FruitSeen));
+                module._lastFruitUTC = originalEventUTC;
+            }
+            else
+            {
+                Log.WriteLine("FruitMsgHandler ignoring stale message. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastFruitUTC.ToString("o"));
+            }
+            return MessageResponse.Completed;
+        }
+        private static async Task<MessageResponse> OnOrientation0MessageReceived(Message msg, object ctx)
+        {
+            AzureModule module = (AzureModule)ctx;
+            var msgBytes = msg.GetBytes();
+            var msgString = Encoding.UTF8.GetString(msgBytes);
+            Log.WriteLine("orientation msg received: '{0}'", msgString);
+            var orientationMsg = JsonConvert.DeserializeObject<OrientationMessage>(msgString);
+            DateTime originalEventUTC = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+            if (orientationMsg.OriginalEventUTCTime != null)
+            {
+                originalEventUTC = DateTime.Parse(orientationMsg.OriginalEventUTCTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+            }
+            if (originalEventUTC >= module._lastOrientation0UTC)
+            {
+                Log.WriteLine("OrientationMsgHandler invoking event. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastOrientation0UTC.ToString("o"));
+                await Task.Run(() => module.Orientation0Changed?.Invoke(module, orientationMsg.OrientationState));
+                module._lastOrientation0UTC = originalEventUTC;
+            }
+            else
+            {
+                Log.WriteLine("Orientation0MsgHandler ignoring stale message. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastOrientation0UTC.ToString("o"));
+            }
+            return MessageResponse.Completed;
+        }
+        private static async Task<MessageResponse> OnOrientation1MessageReceived(Message msg, object ctx)
+        {
+            AzureModule module = (AzureModule)ctx;
+            var msgBytes = msg.GetBytes();
+            var msgString = Encoding.UTF8.GetString(msgBytes);
+            Log.WriteLine("orientation msg received: '{0}'", msgString);
+            var orientationMsg = JsonConvert.DeserializeObject<OrientationMessage>(msgString);
+            DateTime originalEventUTC = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+            if (orientationMsg.OriginalEventUTCTime != null)
+            {
+                originalEventUTC = DateTime.Parse(orientationMsg.OriginalEventUTCTime, null, System.Globalization.DateTimeStyles.RoundtripKind);
+            }
+            if (originalEventUTC >= module._lastOrientation1UTC)
+            {
+                Log.WriteLine("OrientationMsgHandler invoking event. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastOrientation1UTC.ToString("o"));
+                await Task.Run(() => module.Orientation1Changed?.Invoke(module, orientationMsg.OrientationState));
+                module._lastOrientation1UTC = originalEventUTC;
+            }
+            else
+            {
+                Log.WriteLine("Orientation1MsgHandler ignoring stale message. original event UTC {0} prev {1}", originalEventUTC.ToString("o"), module._lastOrientation1UTC.ToString("o"));
+            }
+            return MessageResponse.Completed;
+        }
+        public override async Task OnConnectionChanged(ConnectionStatus status, ConnectionStatusChangeReason reason)
+        {
+            await base.OnConnectionChanged(status, reason);
+            Log.WriteLine("derived connection changed.  status {0} reason {1}", status, reason);
+            return;
+        }
+        protected override async Task OnDesiredModulePropertyChanged(TwinCollection newDesiredProperties)
+        {
+            Log.WriteLine("derived desired properties contains {0} properties", newDesiredProperties.Count);
+            await base.OnDesiredModulePropertyChanged(newDesiredProperties);
+            DesiredPropertiesType<ConfigurationType> dp;
+            if (!newDesiredProperties.Contains(Keys.Configuration))
+            {
+                Log.WriteLine("derived desired properties contains no configuration.  skipping...");
+                return;
+            }
+
+            dp.Configuration = ((JObject)newDesiredProperties[Keys.Configuration]).ToObject<ConfigurationType>();
+            Log.WriteLine("checking for update current desiredProperties {0} new dp {1}", _desiredProperties.ToString(), dp.ToString());
+            var changed = _desiredProperties.Update(dp);
+            if (changed) {
+                Log.WriteLine("desired properties {0} different then current properties, notifying...", _desiredProperties.ToString());
+                ConfigurationChanged?.Invoke(this, dp.Configuration);
+                Log.WriteLine("local notification complete. updating reported properties to cloud twin");
+                await UpdateReportedPropertiesAsync(new KeyValuePair<string, Object>(Keys.Configuration, JsonConvert.SerializeObject(_desiredProperties.Configuration))).ConfigureAwait(false);
+
+            }
+            if (newDesiredProperties.Contains(Keys.FruitTest))
+            {
+                var fruit = (string)((JValue)newDesiredProperties[Keys.FruitTest]).Value;
+                Log.WriteLine("fruittest {0}", fruit != null ? fruit : "(null)");
+                if (fruit != null)
+                {
+                    Log.WriteLine("setting fruit {0}", fruit);
+                    FruitChanged?.Invoke(this, fruit);
+                }
+            }
+            Log.WriteLine("update complete -- current properties {0}", _desiredProperties.ToString());
+        }
+
+        public AzureModule()
+        {
+            _lastFruitUTC = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+            _lastOrientation0UTC = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+            _lastOrientation1UTC = DateTime.SpecifyKind(DateTime.MinValue, DateTimeKind.Utc);
+        }
+        public override async Task AzureModuleInitAsync<C>(C c) 
+        {
+            AzureConnection c1 = c as AzureConnection;
+            await base.AzureModuleInitAsync(c1);
+            await _moduleClient.SetInputMessageHandlerAsync(Keys.InputFruit, OnFruitMessageReceived, this);
+            await _moduleClient.SetInputMessageHandlerAsync(Keys.InputOrientation0, OnOrientation0MessageReceived, this);
+            await _moduleClient.SetInputMessageHandlerAsync(Keys.InputOrientation1, OnOrientation1MessageReceived, this);
+            await _moduleClient.SetMethodHandlerAsync(Keys.SetFruit, SetFruit, this);
+            await base.AzureModuleInitEndAsync();
+        }
+    }
+
+
+    class AzureConnection : AzureConnectionBase
+    {
+        //private MessageHandler _inputMessageHandler { get; set; }
+        public AzureConnection()
+        {
+            
+        }
+        // private async Task<MessageResponse> OnInputMessageReceived(Message msg, object ctx)
+        // {
+        // 
+        // }
+        public static async Task<AzureConnection> CreateAzureConnectionAsync() {
+            return await CreateAzureConnectionAsync<AzureConnection, AzureDevice, AzureModule>();
+        }
+
+        public async Task NotifyModuleLoadAsync()
+        {
+            await Task.WhenAll(
+                Task.Run(async () => await NotifyModuleLoadAsync(Keys.ModuleLoadOutputRouteLocal0, Keys.GPIOModuleId)),
+                Task.Run(async () => await NotifyModuleLoadAsync(Keys.ModuleLoadOutputRouteLocal1, Keys.GPIOModuleId)),
+                Task.Run(async () => await NotifyModuleLoadAsync(Keys.ModuleLoadOutputRouteUpstream, Keys.GPIOModuleId))
+            );
+            Log.WriteLine("derived Module Load D2C message fired");
+        }
+    }
+}
---
+++ Samples/EdgeModules/pwm/CS/Program.cs
@@ -0,0 +1,156 @@
+﻿//
+// Copyright (c) Microsoft. All rights reserved.
+//
+
+using EdgeModuleSamples.Common.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Windows.Foundation;
+
+namespace GPIOFruit
+{
+    class Program
+    {
+        static async Task<int> MainAsync(string[] args)
+        {
+            Log.WriteLine("Starting async...");
+            var Options = new AppOptions();
+
+            Options.Parse(args);
+            Log.Enabled = !Options.Quiet;
+            Log.Verbose = Options.Verbose;
+            Log.WriteLine("arg parse complete...");
+            Dictionary<string, string> FruitColors = new Dictionary<string, string>()
+            {
+                {"apple", "red" },
+                {"pear", "yellow" },
+                {"pen", "green" },
+                {"grapes", "blue"}
+            };
+            AzureConnection connection = null;
+            GPIODevice gpio = null;
+            await Task.WhenAll(
+                Task.Run(async () => {
+                    try { 
+                        if (!Options.Test)
+                        {
+                            Log.WriteLine("starting connection creation");
+                            connection = await AzureConnection.CreateAzureConnectionAsync();
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        Log.WriteLine("GPIO Main CreateAzureConnectionAsync exception {0}", e.ToString());
+                    }
+                }),
+                Task.Run(() =>
+                    {
+                        try
+                        {
+                            gpio = new GPIODevice();
+                            gpio.InitOutputPins(Options);
+                            if (Options.Test)
+                            {
+                                Log.WriteLine("initiating pin test");
+                                if (Options.TestTime.HasValue)
+                                {
+                                    gpio.Test(Options.TestTime.Value, TimeSpan.FromSeconds(2));
+                                } else
+                                {
+                                    gpio.Test(TimeSpan.FromSeconds(15), TimeSpan.FromSeconds(2));
+                                }
+                            }
+                        }
+                        catch (Exception e)
+                        {
+                            Log.WriteLine("GPIO InitOutputPins exception {0}", e.ToString());
+                        }
+                    }
+                )
+            );
+            AzureModule m = (AzureModule)connection.Module;
+            m.ConfigurationChanged += async (object sender, ConfigurationType newConfiguration) =>
+            {
+                var module = (AzureModule)sender;
+                Log.WriteLine("updating gpio pin config with {0}", newConfiguration.ToString());
+                await gpio.UpdatePinConfigurationAsync(newConfiguration.GpioPins);
+            };
+            m.FruitChanged += async (object sender, string fruit) =>
+            {
+                Log.WriteLine("fruit changed to {0}", fruit.ToLower());
+                var module = (AzureModule)sender;
+                await Task.Run(() => gpio.ActivePin = FruitColors[fruit.ToLower()]);
+            };
+            m.Orientation0Changed += async (object sender, EdgeModuleSamples.Common.Orientation o) =>
+            {
+                Log.WriteLine("orientation0 changed to {0}", o.ToString());
+                var module = (AzureModule)sender;
+                await Task.Run(() => gpio.InvertOutputPins());
+            };
+            m.Orientation1Changed += async (object sender, EdgeModuleSamples.Common.Orientation o) =>
+            {
+                Log.WriteLine("orientation1 changed to {0}", o.ToString());
+                var module = (AzureModule)sender;
+                await Task.Run(() => gpio.Blink());
+            };
+            await Task.Run(async () =>
+            {
+                try { 
+                    Log.WriteLine("initializing gpio pin config with {0}", m.Configuration.GpioPins);
+                    await gpio.UpdatePinConfigurationAsync(m.Configuration.GpioPins);
+                }
+                catch (Exception e)
+                {
+                    Log.WriteLine("GPIO UpdatePinConfig Lambda exception {0}", e.ToString());
+                }
+            });
+            await connection.NotifyModuleLoadAsync();
+
+            Log.WriteLine("Initialization Complete. have connection and device pins.  Active Pin is {0}", gpio.ActivePin == null ? "(null)" : gpio.ActivePin);
+
+            Task.WaitAll(Task.Run(() =>
+            {
+                try { 
+                    for (; ; )
+                    {
+                        Log.WriteLine("{0} wait spin", Environment.TickCount);
+                        gpio.LogInputPins();
+                        Thread.Sleep(TimeSpan.FromSeconds(30));
+                    }
+                }
+                catch (Exception e)
+                {
+                    Log.WriteLine("GPIO wait spin exception {0}", e.ToString());
+                }
+
+            }));
+            return 0;
+        }
+
+        static int Main(string[] args)
+        {
+            Log.Enabled = true;
+            Log.WriteLine("Starting...");
+            int rc = 0;
+            try
+            {
+                Task.WaitAll(Task.Run(async () =>
+                    rc = await MainAsync(args))
+                );
+            }
+            catch (Exception e)
+            {
+                Log.WriteLineError("app failed {0}", e.ToString());
+                rc = 1;
+            }
+            Log.WriteLine("Complete....");
+            Console.Out.Flush();
+            return rc;
+
+        }
+    }
+}
