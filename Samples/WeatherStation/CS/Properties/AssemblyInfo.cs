c046c491a5fd4c9e3e37135dc500f137dbada2cb
Sara <saraclay@users.noreply.github.com>
2019-08-16 14:43:16 -0700

    Update AssemblyInfo.cs

--- Samples/WeatherStation/CS/Properties/AssemblyInfo.cs
+++ Samples/WeatherStation/CS/Properties/AssemblyInfo.cs
@@ -1,18 +1,15 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-
-
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WeatherStation")]
+[assembly: AssemblyTitle("WeatherDataReporter")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WeatherStation")]
+[assembly: AssemblyProduct("WeatherDataReporter")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
